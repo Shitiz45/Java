7aa35d9317832b6995c5e97693b15d6324a4ef38
Shitiz Rajvanshi <shitiz45rajvanshi.com>
2025-07-30 08:53:29 +0530

    Constructor overloading

--- OOPs/mypackage/Main.java
+++ OOPs/mypackage/Main.java
@@ -10,7 +10,25 @@ public class Main {
         st1.marks = 85;
 
         st1.study();
-
         st1.showDetails();
+
+        Student st2 = new Student(456, "Shyam", "Mumbai", 90);
+        st2.study();
+        st2.showDetails();
+
+        Student st3 = new Student(789, "Sita", "Bangalore", 95);
+        st3.study();
+        st3.showDetails();
+
+        Student st4 = new Student(101, "Gita");
+        st4.studentCity = "Chennai";
+        st4.marks = 88;
+        st4.study();
+        st4.showDetails();
+
+        Student st5 = new Student(102, "Laxman");
+        st5.marks = 92;
+        st5.study();
+        st5.showDetails();
     }
 }
